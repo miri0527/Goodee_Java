f92154a3ead9d4d67700b39bc3c1c2cfcd93d7ce
miri <miri6507@naver.com>
2023-01-03 17:37:30 +0900

    생성자 매개변수

--- src/com/iu/s4/objects2/CarMain.java
+++ src/com/iu/s4/objects2/CarMain.java
@@ -1,12 +1,10 @@
 package com.iu.s4.objects2;
 
-import java.util.Scanner;
-
 public class CarMain {
 
 	public static void main(String[] args) {
 	
-		//Car c1 = new Car(); -> 에러 개발자가 생성자를 하나 이상 만들면 컴파일러는 자동으로 생성자를 생성하지 않는다
+		Car c1 = new Car(); //-> 에러 개발자가 생성자를 하나 이상 만들면 컴파일러는 자동으로 생성자를 생성하지 않는다
 		
 		c1.info();
 		

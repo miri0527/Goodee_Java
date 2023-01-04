7315ade49980ff896c4701578c7c8a27d848ca48
miri <miri6507@naver.com>
2023-01-04 16:39:17 +0900

    상속과 포함관계 구분

---
+++ src/com/iu/s4/objects5/Car.java
@@ -0,0 +1,15 @@
+package com.iu.s4.objects5;
+
+public class Car {
+	
+	Engine engine; 
+	String brand;
+}
+
+//다른 클래스의 멤버를 내가 선언한 멤버처럼 사용하고 싶을 때
+
+//상속
+//자식클래스 is a 부모클래스 
+
+//포함(멤버 변수로 선언)
+//자식클래스 has a 부모클래스 -> 이게 맞는거 이러면 Engine engine 

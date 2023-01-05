a42be8a5282530ebd594ee7164e54a26c770a538
miri <miri6507@naver.com>
2023-01-05 10:47:43 +0900

    fix

--- src/com/iu/s4/objects6/Main.java
+++ src/com/iu/s4/objects6/Main.java
@@ -4,18 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("프로그램 시작");
-		
-		Robot robot = new Robot(); //로봇 몸만 생성
-		ArmLeft armLeft = new ArmLeft(); //왼쪽팔을 행성 armLeft와 robot은 생명주기 다름 -> 약한연결
-		robot.setArmLeft(armLeft); //왼쪽팔 끼워넣기
-		
-		
-		
-		System.out.println(robot.getArmLeft()); 
-		System.out.println("Right: " + robot.getArmRight()); 
-		
-		robot = null; //->로봇객체를 없애는 방법
-		
+	
+		System.out.println("fixRobot에서 작성");
 		System.out.println("프로그램 종료");
 
 	}
